18ee4b786584cb9de609b2e4041d79c09384bb75
Alex Carson <alcarson1023@yahoo.com>
2019-07-01 12:10:07 -0500

    nearly done, needs media queries for mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { WeddingsComponent } from "./weddings/weddings.component";
+import { CinemaComponent } from "./cinema/cinema.component";
+import { ContactComponent } from "./contact/contact.component";
+import { SurpriseComponent } from "./surprise/surprise.component";
+import { MissingPageComponent } from "./missing-page/missing-page.component";
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      [
+        { path: "home", component: HomeComponent },
+        { path: "gallery", component: GalleryComponent },
+        { path: "weddings", component: WeddingsComponent },
+        { path: "cinema", component: CinemaComponent },
+        { path: "contact", component: ContactComponent },
+        { path: "surprise", component: SurpriseComponent },
+        { path: "", redirectTo: "home", pathMatch: "full" },
+        // { path: "**", redirectTo: "missing-page", pathMatch: "full" }
+        { path: "**", component: MissingPageComponent, pathMatch: "full" }
+      ],
+      { enableTracing: true }
+    )
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,39 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialModule } from "./material.module";
+
+import { MatMenuModule } from "@angular/material/menu";
+import { ScrollDispatchModule } from "@angular/cdk/scrolling";
+import { Scroll } from "@angular/router";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+import { WeddingsComponent } from "./weddings/weddings.component";
+import { SurpriseComponent } from "./surprise/surprise.component";
+import { MissingPageComponent } from "./missing-page/missing-page.component";
+
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
+import { CinemaComponent } from './cinema/cinema.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    HomeComponent,
+    ContactComponent,
+    WeddingsComponent,
+    SurpriseComponent,
+    MissingPageComponent,
+    CinemaComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MatMenuModule,
+    ScrollDispatchModule
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
